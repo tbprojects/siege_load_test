4ddf54c53a36364464fcb80929e2930405d7a61b
Boro <boro.selleo@gmail.com>
2011-08-24 13:29:45 +0200

    install fix

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 puts "** Installing Siege Load Test plugin..."
-FileUtils.mkdir_p(File.dirname("/public/testing/assets"))
+FileUtils.mkdir_p File.join(Rails.root.to_s, File.dirname("/public/testing/assets"))
 FileUtils.cp_r File.join(Rails.root.to_s, "/vendor/plugins/siege_load_test/public"), File.join(Rails.root.to_s, "/public/testing/assets")
 puts "** Successfully installed Siege Load Test plugin..."
