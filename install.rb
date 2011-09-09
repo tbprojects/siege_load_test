57d15107152594379ffc0a1b83e76c0c362b0a96
Boro <boro.selleo@gmail.com>
2011-09-09 11:57:19 +0200

    install fix

--- install.rb
+++ install.rb
@@ -1,3 +1,4 @@
+require File.join(Rails.root.to_s, "/vendor/plugins/siege_load_test/lib/siege_load_test.rb")
 puts "** Installing Siege Load Test plugin..."
 FileUtils.mkdir_p File.join(Rails.root.to_s, File.dirname("/public/testing/siege_load_test/assets"))
 FileUtils.cp_r File.join(Rails.root.to_s, "/vendor/plugins/siege_load_test/public"), File.join(Rails.root.to_s, "/public/testing/siege_load_test/assets")
